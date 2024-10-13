837c3c15735e72e94fdc97ce390f37260c156bb2
Alef A. Araujo <3alyef.dev@gmail.com>
2024-10-13 15:11:05 -0300

    feat: renomear

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const ALLOW_ORIGIN = process.env.ALLOW_ORIGIN;
@@ -12,6 +13,20 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // cookies ou sessões
   });
+
+  app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 1000, // 1 minute
+      max: 50, // Limit each IP to 50 requests per `window` (here, per 1 minute)
+      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+      legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+      handler: (_r, res, _n, options) => {
+        res.status(options.statusCode).json({
+          message: 'Too many requests. Wait a minute.',
+        });
+      },
+    }),
+  );
 
   await app.listen(PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
--- src/spotify-artist-name/entities/spotify-artist.entity.ts
+++ src/spotify-artist-name/entities/spotify-artist.entity.ts
@@ -2,28 +2,31 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 class ExternalUrls {
-  @Field(() => String, { description: 'Spotify external link' })
+  @Field(() => String, { description: 'The Spotify URL for the object.' })
   spotify: string;
 }
 
 @ObjectType()
 class Followers {
-  @Field(() => String, { description: 'Href to followers data' })
+  @Field(() => String, {
+    description:
+      'This will always be set to null, as the Web API does not support it at the moment.',
+  })
   href: string | null;
 
-  @Field(() => Int, { description: 'Total number of followers' })
+  @Field(() => Int, { description: 'The total number of followers.' })
   total: number;
 }
 
 @ObjectType()
 class TypeImages {
-  @Field(() => Int, { description: 'Image height' })
+  @Field(() => Int, { description: 'The image height in pixels.' })
   height: number;
 
-  @Field(() => String, { description: 'Image URL' })
+  @Field(() => String, { description: 'The source URL of the image.' })
   url: string;
 
-  @Field(() => Int, { description: 'Image width' })
+  @Field(() => Int, { description: 'The image width in pixels.' })
   width: number;
 }
 
@@ -32,33 +35,48 @@ export class ArtistType {
   @Field(() => [ArtistType], { description: 'List of artists' })
   artistsList: ArtistType[];
 
-  @Field(() => ExternalUrls, { description: 'External URLs for the artist' })
+  @Field(() => ExternalUrls, {
+    description: 'Known external URLs for this artist.',
+  })
   external_urls: ExternalUrls;
 
-  @Field(() => Followers, { description: 'Artist followers' })
+  @Field(() => Followers, {
+    description: 'Information about the followers of the artist.',
+  })
   followers: Followers;
 
-  @Field(() => [String], { description: 'Genres list' })
+  @Field(() => [String], {
+    description:
+      'A list of the genres the artist is associated with. If not yet classified, the array is empty.',
+  })
   genres: string[];
 
-  @Field(() => String, { description: 'Artist URL' })
+  @Field(() => String, {
+    description:
+      'A link to the Web API endpoint providing full details of the artist.',
+  })
   href: string;
 
-  @Field(() => String, { description: 'Artist ID' })
+  @Field(() => String, { description: 'The Spotify ID for the artist.' })
   id: string;
 
-  @Field(() => [TypeImages], { description: 'Artist images' })
+  @Field(() => [TypeImages], {
+    description: 'Images of the artist in various sizes, widest first.',
+  })
   images: TypeImages[];
 
-  @Field(() => String, { description: 'Artist name' })
+  @Field(() => String, { description: 'The name of the artist.' })
   name: string;
 
-  @Field(() => Int, { description: 'Artist popularity' })
+  @Field(() => Int, {
+    description:
+      "The popularity of the artist. The value will be between 0 and 100, with 100 being the most popular. The artist's popularity is calculated from the popularity of all the artist's tracks",
+  })
   popularity: number;
 
-  @Field(() => String, { description: 'Artist type' })
+  @Field(() => String, { description: 'The object type.' })
   type: string;
 
-  @Field(() => String, { description: 'Artist URI' })
+  @Field(() => String, { description: 'The Spotify URI for the artist.' })
   uri: string;
 }
